64ee92503b0a2ea9dbbeacfea6602866068e60b7
Siddharth Tumre <siddharthtumre2@gmail.com>
2022-06-08 16:37:08 +0530

    add netlify folder

--- src/App.js
+++ src/App.js
@@ -1,37 +1,18 @@
 import './App.css'
-import { MapContainer, TileLayer } from 'react-leaflet'
-import { Navbar, Container, NavDropdown, Nav, NavItem } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { MapContainer, TileLayer, ZoomControl } from 'react-leaflet'
+import Navbar from './Navbar'
+
 
 function App() {
   return (
     <>
-      <Navbar variant='light' expand="lg">
-        <Container>
-          <Navbar.Brand><Link to="/"><i className='bi bi-house-door px-2'></i></Link></Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <NavDropdown title="HYDROLOGICAL" id="hydrological-nav-dropdown" className='px-2'>
-                <NavDropdown.Item>Rainfall</NavDropdown.Item>
-                <NavDropdown.Item>Temperature</NavDropdown.Item>
-                <NavDropdown.Item>Humidity</NavDropdown.Item>
-                <NavDropdown.Item>Evaporation</NavDropdown.Item>
-              </NavDropdown>
-              <NavDropdown title="LISFLOOD" id="hydrological-nav-dropdown" className='px-2'>
-                <NavDropdown.Item>Discharge Value</NavDropdown.Item>
-                <NavDropdown.Item>Flood Marking</NavDropdown.Item>
-              </NavDropdown>
-              <NavItem><input type='date' style={{ padding: "8px 5px", border: "none", color: 'rgba(0,0,0,.55)', textTransform: 'uppercase' }}></input></NavItem>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true}>
+      <Navbar />
+      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true} className="w-screen h-screen" zoomControl={false}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
+        <ZoomControl position='bottomright' />
       </MapContainer>
     </>
   );
---
+++ src/Navbar.js
@@ -0,0 +1,249 @@
+/* This example requires Tailwind CSS v2.0+ */
+import React, { useState } from 'react';
+import { Fragment } from 'react'
+import { Popover, Transition } from '@headlessui/react'
+import {
+    MenuIcon,
+    XIcon,
+} from '@heroicons/react/outline'
+import { ChevronDownIcon } from '@heroicons/react/solid'
+import { faCloudRain, faTemperature2, faDroplet, faBacon, faHouseTsunami, faArrowUp19, faCalendarDays } from '@fortawesome/free-solid-svg-icons'
+import { faSnowflake } from '@fortawesome/free-regular-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import DatePicker from "react-datepicker";
+
+import "react-datepicker/dist/react-datepicker.css";
+
+const hydrological = [
+    {
+        name: 'Rainfall',
+        description: 'Get a better understanding of where your traffic is coming from.',
+        href: '#',
+        icon: faCloudRain,
+    },
+    {
+        name: 'Temperature',
+        description: 'Speak directly to your customers in a more meaningful way.',
+        href: '#',
+        icon: faTemperature2,
+    },
+    { name: 'Humidity', description: "Your customers' data will be safe and secure.", href: '#', icon: faDroplet },
+    {
+        name: 'Evaporation',
+        description: "Connect with third-party tools that you're already using.",
+        href: '#',
+        icon: faBacon,
+    },
+]
+const lisflood = [
+    {
+        name: 'Discharge Value',
+        description: 'Get all of your questions answered in our forums or contact support.',
+        href: '#',
+        icon: faArrowUp19,
+    },
+    {
+        name: 'Flood Marking',
+        description: 'Learn how to maximize our platform to get the most out of it.',
+        href: '#',
+        icon: faHouseTsunami,
+    },
+]
+
+function classNames(...classes) {
+    return classes.filter(Boolean).join(' ')
+}
+
+export default function Navbar() {
+    const [startDate, setStartDate] = useState(new Date());
+    return (
+        <Popover className="relative bg-white" style={{ zIndex: '1110' }}>
+            <div className="max-w-full mx-auto px-4 sm:px-24 fixed top-0 left-0 right-0 bg-white">
+                <div className="flex justify-between items-center border-b-2 border-gray-100 py-4 md:justify-start md:space-x-10">
+                    {/* Brand */}
+                    <div className="flex justify-start">
+                        <a href="/">
+                            <span className="sr-only">Workflow</span>
+                            <FontAwesomeIcon icon={faSnowflake} className="h-8 w-auto sm:h-10 text-indigo-600" />
+                        </a>
+                    </div>
+                    {/* ----- */}
+                    {/* Menu Icon for small screens */}
+                    <div className="-mr-2 -my-2 md:hidden">
+                        <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                            <span className="sr-only">Open menu</span>
+                            <MenuIcon className="h-6 w-6" aria-hidden="true" />
+                        </Popover.Button>
+                    </div>
+                    {/* ----- */}
+                    <Popover.Group as="nav" className="hidden md:flex space-x-10">
+                        <Popover className="relative">
+                            {({ open }) => (
+                                <>
+                                    <Popover.Button
+                                        className={classNames(
+                                            open ? 'text-gray-900' : 'text-gray-500',
+                                            'group bg-white rounded-md inline-flex items-center text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
+                                        )}
+                                    >
+                                        <span>Hydrological</span>
+                                        <ChevronDownIcon
+                                            className={classNames(
+                                                open ? 'text-gray-600' : 'text-gray-400',
+                                                'ml-2 h-5 w-5 group-hover:text-gray-500'
+                                            )}
+                                            aria-hidden="true"
+                                        />
+                                    </Popover.Button>
+
+                                    <Transition
+                                        as={Fragment}
+                                        enter="transition ease-out duration-200"
+                                        enterFrom="opacity-0 translate-y-1"
+                                        enterTo="opacity-100 translate-y-0"
+                                        leave="transition ease-in duration-150"
+                                        leaveFrom="opacity-100 translate-y-0"
+                                        leaveTo="opacity-0 translate-y-1"
+                                    >
+                                        <Popover.Panel className="absolute z-10 -ml-4 mt-3 transform px-2 w-screen max-w-md sm:px-0 lg:ml-0 lg:left-1/2 lg:-translate-x-1/2">
+                                            <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
+                                                <div className="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
+                                                    {hydrological.map((item) => (
+                                                        <a
+                                                            key={item.name}
+                                                            href={item.href}
+                                                            className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50"
+                                                        >
+                                                            <FontAwesomeIcon icon={item.icon} className="flex-shrink-0 h-6 w-6 text-indigo-600" aria-hidden="true" />
+                                                            <div className="ml-4">
+                                                                <p className="text-base font-medium text-gray-900">{item.name}</p>
+                                                                <p className="mt-1 text-sm text-gray-500">{item.description}</p>
+                                                            </div>
+                                                        </a>
+                                                    ))}
+                                                </div>
+                                            </div>
+                                        </Popover.Panel>
+                                    </Transition>
+                                </>
+                            )}
+                        </Popover>
+
+                        <Popover className="relative">
+                            {({ open }) => (
+                                <>
+                                    <Popover.Button
+                                        className={classNames(
+                                            open ? 'text-gray-900' : 'text-gray-500',
+                                            'group bg-white rounded-md inline-flex items-center text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
+                                        )}
+                                    >
+                                        <span>LISFLOOD</span>
+                                        <ChevronDownIcon
+                                            className={classNames(
+                                                open ? 'text-gray-600' : 'text-gray-400',
+                                                'ml-2 h-5 w-5 group-hover:text-gray-500'
+                                            )}
+                                            aria-hidden="true"
+                                        />
+                                    </Popover.Button>
+
+                                    <Transition
+                                        as={Fragment}
+                                        enter="transition ease-out duration-200"
+                                        enterFrom="opacity-0 translate-y-1"
+                                        enterTo="opacity-100 translate-y-0"
+                                        leave="transition ease-in duration-150"
+                                        leaveFrom="opacity-100 translate-y-0"
+                                        leaveTo="opacity-0 translate-y-1"
+                                    >
+                                        <Popover.Panel className="absolute z-10 left-1/2 transform -translate-x-1/2 mt-3 px-2 w-screen max-w-md sm:px-0">
+                                            <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
+                                                <div className="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
+                                                    {lisflood.map((item) => (
+                                                        <a
+                                                            key={item.name}
+                                                            href={item.href}
+                                                            className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50"
+                                                        >
+                                                            <FontAwesomeIcon icon={item.icon} className="flex-shrink-0 h-6 w-6 text-indigo-600" aria-hidden="true" />
+                                                            <div className="ml-4">
+                                                                <p className="text-base font-medium text-gray-900">{item.name}</p>
+                                                                <p className="mt-1 text-sm text-gray-500">{item.description}</p>
+                                                            </div>
+                                                        </a>
+                                                    ))}
+                                                </div>
+                                            </div>
+                                        </Popover.Panel>
+                                    </Transition>
+                                </>
+                            )}
+                        </Popover>
+                        <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} className="text-gray-500 group bg-white rounded-md inline-flex items-center text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" />
+                    </Popover.Group>
+                </div>
+            </div>
+
+            <Transition
+                as={Fragment}
+                enter="duration-200 ease-out"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="duration-100 ease-in"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+            >
+                <Popover.Panel focus className="absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden">
+                    <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white divide-y-2 divide-gray-50">
+                        <div className="pt-5 pb-6 px-5">
+                            <div className="flex items-center justify-between">
+                                <div>
+                                    <img
+                                        className="h-8 w-auto"
+                                        src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+                                        alt="Workflow"
+                                    />
+                                </div>
+                                <div className="-mr-2">
+                                    <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                                        <span className="sr-only">Close menu</span>
+                                        <XIcon className="h-6 w-6" aria-hidden="true" />
+                                    </Popover.Button>
+                                </div>
+                            </div>
+                            <div className="mt-6">
+                                <nav className="grid gap-y-8">
+                                    {hydrological.map((item) => (
+                                        <a
+                                            key={item.name}
+                                            href={item.href}
+                                            className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50"
+                                        >
+                                            <FontAwesomeIcon icon={item.icon} className="flex-shrink-0 h-6 w-6 text-indigo-600" aria-hidden="true" />
+                                            <span className="ml-3 text-base font-medium text-gray-900">{item.name}</span>
+                                        </a>
+                                    ))}
+                                    {lisflood.map((item) => (
+                                        <a
+                                            key={item.name}
+                                            href={item.href}
+                                            className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50"
+                                        >
+                                            <FontAwesomeIcon icon={item.icon} className="flex-shrink-0 h-6 w-6 text-indigo-600" aria-hidden="true" />
+                                            <span className="ml-3 text-base font-medium text-gray-900">{item.name}</span>
+                                        </a>
+                                    ))}
+                                    <div className='flex'>
+                                        <FontAwesomeIcon icon={faCalendarDays} className="flex-shrink-0 h-6 w-6 text-indigo-600" aria-hidden="true" />
+                                        <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} className="ml-3 text-base font-medium text-gray-900 group bg-white rounded-md inline-flex items-center focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" />
+                                    </div>
+                                </nav>
+                            </div>
+                        </div>
+                    </div>
+                </Popover.Panel>
+            </Transition>
+        </Popover>
+    )
+}
